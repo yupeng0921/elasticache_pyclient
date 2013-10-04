01311283f2890efb8249d854845cf11c815c225f
yupeng <penyu@amazon.com>
2013-10-04 08:36:46 +0000

    check version

--- elasticache_pyclient/memcache_client.py
+++ elasticache_pyclient/memcache_client.py
@@ -8,8 +8,9 @@ import threading
 import time
 import logging
 import hash_ring
+from distutils.version import StrictVersion
 
-class InvalidConfigurationError(Exception):
+class InvalidTelentReplyError(Exception):
     """
     receive configuration from endpoint is invalide
     """
@@ -31,25 +32,41 @@ class Cluster():
     """
     get the cluster config, store version and node list
     """
-    def __init__(self, server, timeout):
+    def __init__(self, server, timeout, logger=None):
         host, port = server.split(':')
         tn = telnetlib.Telnet(host, port)
-        tn.write('config get cluster\n')
+        tn.write('version\n')
+        ret = tn.read_until('\r\n', 3)
+        if logger:
+            logger.debug('version:\n')
+            logger.debug(ret)
+        version_list = ret.split(' ')
+        if len(version_list) != 2 or version_list[0] != 'VERSION':
+            raise InvalidTelentReplyError(ret)
+        version = version_list[1][0:-2]
+        if StrictVersion(version) >= StrictVersion('1.4.14'):
+            get_cluster = 'config get cluster\n'
+        else:
+            get_cluster = 'get AmazonElastiCache:cluster\n'
+        tn.write(get_cluster)
         ret = tn.read_until('END\r\n',  3)
+        if logger:
+            logger.debug('config:\n')
+            logger.debug(ret)
         tn.close()
         p = re.compile(r'\r?\n')
         conf = p.split(ret)
         if len(conf) != 6:
-            raise InvalidConfigurationError(ret)
-        if not (conf[0][0:14] == 'CONFIG cluster' and conf[4][0:3] == 'END'):
-            raise InvalidConfigurationError(ret)
+            raise InvalidTelentReplyError(ret)
+        if conf[4][0:3] != 'END':
+            raise InvalidTelentReplyError(ret)
         self.version = conf[1]
         self.servers = []
         nodes_str = conf[2].split(' ')
         for node_str in nodes_str:
             node_list = node_str.split('|')
             if len(node_list) != 3:
-                raise InvalidConfigurationError(ret)
+                raise InvalidTelentReplyError(ret)
             self.servers.append(node_list[1] + ':' + node_list[2])
 
 class Timer(threading.Thread):
@@ -109,7 +126,6 @@ class MemcacheClient():
         self.timer.start()
 
     def _extends(self, method_name, method_str):
-        #_method = None
         exec method_str + '''\n_method = %s''' % method_name
         self.__dict__[method_name] = new.instancemethod(_method, self, None)
 
@@ -131,7 +147,8 @@ class MemcacheClient():
 
 if __name__ == '__main__':
     server = 'mytest.lwgyhw.cfg.usw2.cache.amazonaws.com:11211'
+    # server = 'mytest2.lwgyhw.cfg.usw2.cache.amazonaws.com:11211'
     m = MemcacheClient(server, client_debug='test.log')
-    # m.set('xyz', 14)
+    # m.set('xyz', 'bar')
     print m.get('xyz')
-    time.sleep(5)
+    # time.sleep(5)
