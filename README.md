5649c1f5b39e646cd2aa48b47f43af18ed5aca00
yupeng <penyu@amazon.com>
2013-10-06 07:08:10 +0000

    unittest can work

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-do auto discovery for aws elasticache
+# do auto discovery for aws elasticache
+
+## introduce
+Implement aws elasticache auto discovery, for detail about auto discovery, please reference to:
+http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/AutoDiscovery.html
+
+It uses python-memcached implements memcache command, and use hash_ring implements consistent hash, below links have more detail about python-memcached and hash_ring:
+
+https://pypi.python.org/pypi/python-memcached
+https://pypi.python.org/pypi/hash_ring/
+
+## install
+
+pip install elasticache_pyclient
+or
+python-pip install elasticache_pyclient
+
+The python-memcached and hash_ring packages will also be installed automatically
+
+## usage
+
+It support all the python-memcached methods, to examine all the python-memcached methods, you can run these commands in python interpreter:
+>>> import memcache
+>>> help(memcache.Client)
--- elasticache_pyclient/memcache_client.py
+++ elasticache_pyclient/memcache_client.py
@@ -139,15 +139,21 @@ class MemcacheClient():
         self.__dict__[method_name] = new.instancemethod(_method, self, None)
 
     def _update(self):
-        cluster = Cluster(self.server, self.auotdiscovery_timeout)
+        try:
+            cluster = Cluster(self.server, self.auotdiscovery_timeout)
+        except Exception, e:
+            if self.logger:
+                self.logger.debug(e)
+                self.logger.handlers[0].flush()
+            return
         if cluster.version != self.cluster.version:
-            ret = self.lock.acqurie(True)
+            ret = self.lock.acquire(True)
             if not ret:
                 raise GetLockError('_update')
             if self.logger:
                 self.logger.debug("old: " + self.cluster.version + str(self.cluster.servers))
                 self.logger.debug("new: " + cluster.version + str(cluster.servers))
-                logger.handlers[0].flush()
+                self.logger.handlers[0].flush()
             self.cluster = cluster
             self.need_update = True
             self.lock.release()
@@ -158,63 +164,201 @@ class MemcacheClient():
 if __name__ == '__main__':
     import unittest
     import time
+    import sys
     import socket
     import boto.ec2
     import boto.elasticache
     class MemcacheClientTestCase(unittest.TestCase):
-        def launch_resources(self, name, version, number):
+        resources = []
+        region = 'us-west-2'
+        default_type = 'cache.m1.small'
+        default_number = 4
+        use_exist_cluster = None
+        def __init__(self, new_server, new_name, old_server, old_name, *k, **kw):
+            if new_server and old_server and new_name and old_name:
+                self.new_name = new_name
+                self.new_server = new_server
+                self.old_name = old_name
+                self.old_server = old_server
+                self.conn = boto.elasticache.connect_to_region(self.region)
+                self.use_exist_cluster = True
+            unittest.TestCase.__init__(self, *k, **kw)
+        def setUp(self):
+            if self.use_exist_cluster:
+                return
             print "lauching resources"
-            region = 'us-west-2'
-            default_type = 'cache.m1.small'
+            suffix = time.strftime('%y%m%d%H%M%S', time.localtime())
+            self.new_name = 'new' + suffix
+            self.old_name = 'old' + suffix
+            new_version = '1.4.14'
+            old_version = '1.4.5'
             port = 11211
-            c1 = boto.ec2.connect_to_region(region)
-            self.sg = c1.create_security_group(name+'sg', 'for elasticache test')
+
+            c1 = boto.ec2.connect_to_region(self.region)
+            self.sg = c1.create_security_group('sg_'+suffix, 'for elasticache test')
+            self.resources.append('sg')
             self.sg.authorize('tcp', str(port), str(port), '0.0.0.0/0')
             print 'sg created'
-            self.conn = boto.elasticache.connect_to_region(region)
-            self.conn.create_cache_cluster(name, number, default_type, 'memcached', \
-                                     engine_version=version, security_group_ids=[self.sg.id], port=port)
+
+            self.conn = boto.elasticache.connect_to_region(self.region)
+            self.conn.create_cache_cluster(self.new_name, self.default_number, self.default_type, 'memcached', \
+                                               engine_version=new_version, security_group_ids=[self.sg.id], port=port)
+
+            self.conn.create_cache_cluster(self.old_name, self.default_number, self.default_type, 'memcached', \
+                                               engine_version=old_version, security_group_ids=[self.sg.id], port=port)
+
             print 'waiting for cluster available'
+
+            while True:
+                ret = self.conn.describe_cache_clusters(self.new_name)
+                status = ret['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]['CacheClusterStatus']
+                if status == 'available':
+                    break
+                time.sleep(1)
+            self.resources.append('new_memcached')
+            endpoint = ret['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]['ConfigurationEndpoint']
+            self.new_server = str(endpoint['Address'] + ':' + str(endpoint['Port']))
+            print self.new_server
+
+            while True:
+                ret = self.conn.describe_cache_clusters(self.old_name)
+                status = ret['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]['CacheClusterStatus']
+                if status == 'available':
+                    break
+                time.sleep(1)
+            self.resources.append('old_memcached')
+            endpoint = ret['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]['ConfigurationEndpoint']
+            self.old_server = str(endpoint['Address'] + ':' + str(endpoint['Port']))
+            print self.old_server
+
+        def tearDown(self):
+            if self.use_exist_cluster:
+                return
+            print 'clean resoures'
+            if 'old_memcached' in self.resources:
+                self.conn.delete_cache_cluster(self.old_name)
+                print 'deleted old memcached'
+            if 'new_memcached' in self.resources:
+                self.conn.delete_cache_cluster(self.new_name)
+                print 'deleted new memcached'
+            if 'sg' in self.resources:
+                while True:
+                    try:
+                        self.sg.delete()
+                    except boto.exception.EC2ResponseError, e:
+                        time.sleep(10)
+                        continue
+                    print 'deleted sg'
+                    break
+        def run_test(self, server, name, client_debug):
+            print 'start testing'
+            print server
+            print name
+            print client_debug
+            retry = 300
+            while retry >= 0:
+                try:
+                    m = MemcacheClient(server, client_debug=client_debug)
+                except socket.gaierror, e:
+                    time.sleep(10)
+                    retry -= 1
+                    continue
+                break
+            self.assertTrue(retry >= 0)
+            print 'got MemcacheClient'
+
+            total_count = 1000
+            for i in range(0, total_count):
+                m.set(str(i), i)
+            for i in range(0, total_count):
+                ret = m.get(str(i))
+                self.assertEquals(ret, i)
+
+            print 'increasing nodes'
+            self.conn.modify_cache_cluster(name, num_cache_nodes=self.default_number+1, apply_immediately=True)
+            time.sleep(10)
             while True:
                 ret = self.conn.describe_cache_clusters(name)
                 status = ret['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]['CacheClusterStatus']
                 if status == 'available':
                     break
                 time.sleep(1)
-            endpoint = ret['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]['ConfigurationEndpoint']
-            link = str(endpoint['Address'] + ':' + str(endpoint['Port']))
-            print 'link: ' + link
-            return link
-        def test_new_version_without_debug(self):
-            server = self.launch_resources('newversiontest2', '1.4.14', 5)
-            s1 = 'newversiontest2.lwgyhw.cfg.usw2.cache.amazonaws.com:11211'
-            if server == s1:
-                print 'equal'
-            else:
-                print 'Not equal'
-            server = s1
-            retry = 300
-            while retry >= 0:
-                try:
-                    m = MemcacheClient(server, client_debug='test.log')
-                except socket.gaierror, e:
-                    print e
-                    print "sleep and retry"
-                    print retry
-                    time.sleep(10)
-                    retry -= 1
-                    continue
-                break
-            assert retry >= 0
-            # server = 'nelwversiontest.lwgyhw.cfg.usw2.cache.amazonaws.com:11211'
-            m.set('foo', 'bar')
-            print m.get('foo')
+            print 'increased nodes'
 
+            correct_count = 0
+            for i in range(0, total_count):
+                ret = m.get(str(i))
+                if ret:
+                    self.assertEquals(ret, i)
+                    correct_count += 1
+
+            print 'total: %d' % total_count
+            print 'correct: %d' % correct_count
+            self.assertTrue(correct_count > total_count / 2)
+
+            time.sleep(90)
+
+            correct_count = 0
+            for i in range(0, total_count):
+                ret = m.get(str(i))
+                if ret:
+                    self.assertEquals(ret, i)
+                    correct_count += 1
+
+            print 'total: %d' % total_count
+            print 'correct: %d' % correct_count
+            self.assertTrue(correct_count > total_count / 2)
+
+            for i in range(0, total_count):
+                m.set(str(i+total_count), i+total_count)
+            for i in range(0, total_count):
+                ret = m.get(str(i+total_count))
+                self.assertEquals(ret, i+total_count)
+
+            print 'decreasing nodes'
+            ret = self.conn.describe_cache_clusters(cache_cluster_id=name, show_cache_node_info=True)
+            node_id = ret['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]['CacheNodes'][0]['CacheNodeId']
+            self.conn.modify_cache_cluster(name, num_cache_nodes=self.default_number, cache_node_ids_to_remove=[node_id], apply_immediately=True)
+            time.sleep(10)
+            while True:
+                ret = self.conn.describe_cache_clusters(name)
+                status = ret['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]['CacheClusterStatus']
+                if status == 'available':
+                    break
+                time.sleep(1)
+            print 'decreased nodes'
+
+            time.sleep(90)
+
+            correct_count = 0
+            for i in range(0, total_count):
+                ret = m.get(str(i+total_count))
+                if ret:
+                    self.assertEquals(ret, i+total_count)
+                    correct_count += 1
+
+            print 'total: %d' % total_count
+            print 'correct: %d' % correct_count
+            self.assertTrue(correct_count > total_count / 2)
+
+            print 'completed'
+
+
+        def run_all_test(self):
+            self.run_test(self.new_server, self.new_name, None)
+            self.run_test(self.old_server, self.old_name, None)
+
+    if (len(sys.argv) == 5):
+        new_server = sys.argv[1]
+        new_name = sys.argv[2]
+        old_server = sys.argv[3]
+        old_name = sys.argv[4]
+    else:
+        new_server = None
+        new_name = None
+        old_server = None
+        old_name = None
     suite = unittest.TestSuite()
-    suite.addTest(MemcacheClientTestCase('test_new_version_without_debug'))
+    suite.addTest(MemcacheClientTestCase(new_server, new_name, old_server, old_name, 'run_all_test'))
     runner = unittest.TextTestRunner()
     runner.run(suite)
-    # server = 'newversiontest.lwgyhw.cfg.usw2.cache.amazonaws.com:11211'
-    # m = MemcacheClient(server)
-    # m.set('foo', 'bar')
-    # print m.get('foo')
