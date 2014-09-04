42bd486002096d3734105cabb25520f98442fe8d
yupeng <penyu@amazon.com>
2014-09-04 14:00:13 +0000

    log can work

--- elasticache_pyclient/__init__.py
+++ elasticache_pyclient/__init__.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python
 
+import logging
+
+__all__ = ['MemcacheClient', 'elasticache_logger']
 __version__ = "1.0.1"
+
+elasticache_logger = logging.getLogger('elasticache_logger')
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+elasticache_logger.addHandler(NullHandler())
+
 try:
     from memcache_client import MemcacheClient
 except ImportError, e:
