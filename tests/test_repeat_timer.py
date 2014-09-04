84ee5f642051f166dd88bf5a02c1c0554997f364
yupeng <penyu@amazon.com>
2014-09-04 14:56:24 +0000

    use mock for TestRepeatTimer

--- tests/test_repeat_timer.py
+++ tests/test_repeat_timer.py
@@ -2,18 +2,17 @@
 
 import time
 import unittest
+from mock import Mock
 from elasticache_pyclient.repeat_timer import RepeatTimer
 
 class TestRepeatTimer(unittest.TestCase):
     def setUp(self):
-        self.count = 0
-        def test_func():
-            self.count += 1
-        self.timer = RepeatTimer('test_timer', 1, test_func)
+        self.mock_func = Mock()
+        self.timer = RepeatTimer('test_timer', 1, self.mock_func)
     def test_run_timer(self):
         self.timer.start()
         time.sleep(3)
-        self.assertTrue(self.count >= 2)
+        self.assertTrue(self.mock_func.call_count >= 2)
     def tearDown(self):
         self.timer.stop_timer()
         self.timer.join()
