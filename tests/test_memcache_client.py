61dccee76b1a08c57f3ecdebe8a16018e9c08b64
yupeng <yupeng0921@gmail.com>
2018-09-30 16:46:39 +0000

    add tox

---
+++ tests/test_memcache_client.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import os
+
+from elasticache_pyclient import MemcacheClient
+
+
+class MemcacheClientTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_client(self):
+        ecache_url=os.environ['ECACHE_URL']
+        mc = MemcacheClient(ecache_url)
+        mc.set('foo', 'bar')
+        self.assertEqual(mc.get('foo'), 'bar')
