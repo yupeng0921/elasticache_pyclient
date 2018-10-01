0ef374672675b32dd32b55f948dc50c606160f2d
yupeng <yupeng0921@gmail.com>
2018-10-01 05:42:31 +0000

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 PACKAGE = "elasticache_pyclient"
 NAME = "elasticache_pyclient"
 KEYWORDS = ("aws", "ealsticache")
-VERSION = '2.1'
+VERSION = '2.1.1'
 DESCRIPTION = "pythone client for elasticache auto discovery"
 LICENSE = 'LGPL'
 URL = "https://github.com/yupeng820921/elasticache_pyclient"
